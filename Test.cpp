0010155a37de3ad8d7cea887820eb3dd5ebc7466
DumbDev69420 <116299301+DumbDev69420@users.noreply.github.com>
2023-10-09 15:30:42 +0200

    Add files via upload

---
+++ Test.cpp
@@ -0,0 +1,81 @@
+#include "src/overlay.h"
+#include <chrono>
+#include <stdio.h>
+#include <stdlib.h>
+#include <thread>
+#include "icl.h"
+#include "proc.h"
+
+#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+
+
+
+//Base hook by iraizo on github: https://github.com/iraizo/nvidia-overlay-hijack
+
+
+
+
+
+void render() {}
+
+
+
+
+int main()
+{
+    Overlay* overlay = new Overlay(L"Signika Negative", 28.0, 15.0);
+
+    if (!overlay->init())
+        return 1;
+    if (!overlay->startup_d2d())
+        return 1;
+
+    bool DifText = false;
+    bool Open = false;
+
+
+    
+
+
+    const char* WaterMarkText = "ScuffedGUI Made by Senpai42";
+
+    while (!overlay->Quit && !GetAsyncKeyState(VK_DELETE))
+    {
+        
+
+        GetCursorPos(&overlay->MousePosF);
+
+        overlay->begin_scene();
+        overlay->clear_scene();
+        if (Open == true) {
+            
+            overlay->DrawMenu(D2D1::ColorF(D2D1::ColorF::Blue)); {
+                if (overlay->Button("Exit  ")) {
+                    overlay->Quit = true;
+                }
+
+            }
+
+
+            
+            
+        }
+
+
+        overlay->draw_text(10, 10, WaterMarkText, D2D1::ColorF(D2D1::ColorF::Green));
+
+        overlay->end_scene();
+
+
+        if (GetAsyncKeyState(VK_INSERT)& 1) {
+            Open = !Open;
+        }
+
+    }
+
+
+
+    delete overlay;
+
+    return 0;
+}
---
+++ icl.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <Windows.h>
+#include <vector>
+#include <TlHelp32.h>
+#include <Psapi.h>
+#include <iostream>
